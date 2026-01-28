872e7eb28c4e22dc1b257d7967c20b97307c3b01
Matthew Russell <matthewjosephrussell@gmail.com>
2026-01-27 21:07:36 -0800

    fix: hassfest manifest order + ruff import sort

--- custom_components/beestat/config_flow.py
+++ custom_components/beestat/config_flow.py
@@ -4,12 +4,11 @@ from __future__ import annotations
 import logging
 from typing import Any
 
-import voluptuous as vol
-
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+import voluptuous as vol
 
 from .api import BeestatApiClient, BeestatApiError
 from .const import (
